f6479d3b18d7125ae55104cc6223b65861ca6cdf
Uyen Truong <haiuyentruong@gmail.com>
2019-02-19 22:23:13 +0200

    Refactor: improve code

--- utshp.c
+++ utshp.c
@@ -30,10 +30,9 @@ void        parse_int32(unsigned char *buf, int *p, int n, int big_endian);
 void        parse_double(unsigned char *buf, double *p, int n);
 int         record_prepend(Record **p_head, Record *new_record);
 void        record_reverse(Record **p_head);
-int         record_len(Record *head);
+int         record_length(Record *head);
 void        record_free(Record *head);
 void        record_nth_print(Record *head, int n);
-
 const char *shape_type(int x);
 
 int main(void)
@@ -45,13 +44,13 @@ int main(void)
 
     parse_header(fp);
 
-    while (record_prepend(&record, parse_record(fp)) == 1)
-        continue;
+    while (record_prepend(&record, parse_record(fp)) == 0)
+        ;
     record_reverse(&record);
 
     while (1)
     {
-        printf("Enter record number: ");
+        printf("Enter record number ('q' to quit): ");
         scanf("%s", buf);
         n = atoi(buf);
         if (n == 0)
@@ -77,7 +76,6 @@ void parse_header(FILE *fp)
     parse_int32(header + 28, vs, 2, 0);         // Parse version, shapetype
     parse_double(header + 36, ranges, 8);       // Parse boundary ranges
 
-
     printf("File code  \t%d\n", code);
     printf("File length\t%d\n", length * 2);
     printf("Version    \t%d\n", vs[0]);
@@ -92,24 +90,25 @@ Record *parse_record(FILE *fp)
 {
     unsigned char header[8];
     int           num_len[2];
-    Record       *record = malloc(sizeof(Record));
 
-    int check = fread(header, sizeof(header), 1, fp);
-    if (check != 1)
+    int nitems = fread(header, sizeof(header), 1, fp);
+    if (nitems != 1)
         return NULL;
 
     parse_int32(header, num_len, 2, 1);             // Parse record number, length
 
     unsigned char content[num_len[1] * 2];
     int           shape_type;
+
     fread(content, sizeof(content), 1, fp);         // Read content of record
     parse_int32(content, &shape_type, 1, 0);        // Parse shapetype
 
-    record->num = num_len[0];
-    record->len = num_len[1] * 2;
+    Record *record     = malloc(sizeof(Record));
+    record->num        = num_len[0];
+    record->len        = num_len[1] * 2;
     record->shape_type = shape_type;
-    record->polyline = parse_polyline(content + 4); // Parse PolyLine
-    record->next = NULL;
+    record->polyline   = parse_polyline(content + 4); // Parse PolyLine
+    record->next       = NULL;
 
     return record;
 }
@@ -117,9 +116,9 @@ Record *parse_record(FILE *fp)
 PolyLine *parse_polyline(unsigned char *buf)
 {
     PolyLine *polyline = malloc(sizeof(PolyLine));
-    double box[4];
-    int parts_points[2];
-    int i;
+    double    box[4];
+    int       parts_points[2];
+    int       i;
 
     parse_double(buf, box, 4);
     parse_int32(buf + 32, parts_points, 2, 0);
@@ -128,15 +127,10 @@ PolyLine *parse_polyline(unsigned char *buf)
         polyline->box[i] = box[i];
     polyline->num_parts  = parts_points[0];
     polyline->num_points = parts_points[1];
-
-    int parts[polyline->num_parts];
-    polyline->parts = malloc(sizeof(int) * polyline->num_parts);
-    parse_int32(buf + 40, parts, polyline->num_parts, 0);
-    for (i = 0; i < polyline->num_parts; i++)
-        polyline->parts[i] = parts[i];
-
-    polyline->points = parse_points(buf + 40 + (4 * polyline->num_parts),
-                                    polyline->num_points);
+    polyline->parts      = malloc(sizeof(int) * polyline->num_parts);
+    parse_int32(buf + 40, polyline->parts, polyline->num_parts, 0);
+    polyline->points     = parse_points(buf + 40 + (4 * polyline->num_parts),
+                                        polyline->num_points);
 
     return polyline;
 }
@@ -144,7 +138,10 @@ PolyLine *parse_polyline(unsigned char *buf)
 Point *parse_points(unsigned char *buf, int num_points)
 {
     Point *points = malloc(sizeof(Point) * num_points);
-
+#if 1
+    // Almost safe. Not exactly portable: only works on 64-bit machines
+    parse_double(buf, (double *)points, num_points * 2);
+#else
     for (int i = 0; i < num_points; i++, buf += 16)
     {
         double xy[2];
@@ -152,7 +149,7 @@ Point *parse_points(unsigned char *buf, int num_points)
         points[i].x = xy[0];
         points[i].y = xy[1];
     }
-
+#endif
     return points;
 }
 
@@ -186,14 +183,13 @@ void parse_double(unsigned char *buf, double *p, int n)
 
 int record_prepend(Record **p_head, Record *new_record)
 {
-    int result = 1;
     if (new_record == NULL)
-        return 0;
+        return -1;
 
     new_record->next = *p_head;
     *p_head = new_record;
 
-    return result;
+    return 0;
 }
 
 void record_reverse(Record **p_head)
@@ -213,7 +209,7 @@ void record_reverse(Record **p_head)
     *p_head = prev;
 }
 
-int record_len(Record *head)
+int record_length(Record *head)
 {
     int len = 0;
 
@@ -238,44 +234,41 @@ void record_free(Record *head)
 void record_nth_print(Record *head, int n)
 {
     Record *record = head;
-    int value = 1;
-    int i;
+    int     i;
 
-    if (n >= record_len(head))
-        value = 0;
+    if (n >= record_length(head))
+    {
+        printf("This record doesn't exist\n");
+        return;
+    }
     else
     {
         for (i = 0; i < n - 1; i++)
             record = record->next;
     }
 
-    if (value == 0)
-        printf("This record doesn't exist\n");
-    else
+    printf("Record number\t%d\n", record->num);
+    printf("Record length\t%d\n", record->len);
+    printf("Shape type \t%d (%s)\n", record->shape_type,
+            shape_type(record->shape_type));
+
+    printf("Box Xmin, Ymin, Xmax, Ymax\n");
+    for (i = 0; i < 4; i++)
+        printf("%f\t", record->polyline->box[i]);
+    printf("\n");
+
+    printf("NumParts\t%d\n", record->polyline->num_parts);
+    printf("NumPoints\t%d\n", record->polyline->num_points);
+
+    printf("Parts    \t");
+    for (i = 0; i < record->polyline->num_parts; i++)
+        printf("%d ", record->polyline->parts[i]);
+    printf("\n");
+
+    for (i = 0; i < record->polyline->num_points; i++)
     {
-        printf("Record number\t%d\n", record->num);
-        printf("Record length\t%d\n", record->len);
-        printf("Shape type \t%d (%s)\n", record->shape_type,
-                shape_type(record->shape_type));
-
-        printf("Box Xmin, Ymin, Xmax, Ymax\n");
-        for (i = 0; i < 4; i++)
-            printf("%f\t", record->polyline->box[i]);
-        printf("\n");
-
-        printf("NumParts\t%d\n", record->polyline->num_parts);
-        printf("NumPoints\t%d\n", record->polyline->num_points);
-
-        printf("Parts    \t");
-        for (i = 0; i < record->polyline->num_parts; i++)
-            printf("%d ", record->polyline->parts[i]);
-        printf("\n");
-
-        for (i = 0; i < record->polyline->num_points; i++)
-        {
-            printf("Point[%d]\tX %f\t Y %f\n", i, record->polyline->points[i].x,
-                                                 record->polyline->points[i].y);
-        }
+        printf("Point[%d]\tX %f\tY %f\n", i, record->polyline->points[i].x,
+                                             record->polyline->points[i].y);
     }
 }
 
