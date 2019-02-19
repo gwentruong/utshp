99edd0a45aed5765c4d0f72905741c6301df5f70
Uyen Truong <haiuyentruong@gmail.com>
2019-02-19 11:03:28 +0200

    Print record and shape content

--- utshp.c
+++ utshp.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 typedef struct {
-    double box[4];
-    int    num_parts;
-    int    num_points;
-    int   *parts;
-    int   *points;
-} polyline;
+    double x;
+    double y;
+} Point;
+
+typedef struct {
+    double  box[4];
+    int     num_parts;
+    int     num_points;
+    int    *parts;
+    Point  *points;
+} PolyLine;
 
 const char *print_shape_type(int x);
 void        parse_int32(unsigned char *buf, int *p, int n, int big_endian);
 void        parse_double(unsigned char *buf, double *p, int n);
 void        parse_header(FILE *fp);
 void        parse_record(FILE *fp);
+void        parse_points(unsigned char *buf, int num_points);
 
 int main(void)
 {
@@ -54,19 +60,51 @@ void parse_record(FILE *fp)
 {
     unsigned char header[8];
     fread(header, sizeof(header), 1, fp);
+    int i;
 
     int num_len[2];
     parse_int32(header, num_len, 2, 1);
     printf("Record number\t%d\n", num_len[0]);
-    printf("Record length\t%d\n", num_len[1]);
+    printf("Record length\t%d\n", num_len[1] * 2);
 
-    unsigned char content[num_len[1]];
+    unsigned char content[num_len[1] * 2];
     fread(content, sizeof(content), 1, fp);
 
     int shape_type;
     parse_int32(content, &shape_type, 1, 0);
     printf("Shape type \t%d (%s)\n", shape_type, print_shape_type(shape_type));
 
+    double box[4];
+    parse_double(content + 4, box, 4);
+    printf("Box Xmin, Ymin, Xmax, Ymax\n");
+    for (i = 0; i < 4; i++)
+        printf("%f\t", box[i]);
+    printf("\n");
+
+    int parts_points[2];
+    parse_int32(content + 36, parts_points, 2, 0);
+    printf("NumParts\t%d\n", parts_points[0]);
+    printf("NumPoints\t%d\n", parts_points[1]);
+
+    int parts[parts_points[0]];
+    parse_int32(content + 44, parts, parts_points[0], 0);
+    printf("Parts    \t");
+    for (i = 0; i < parts_points[0]; i++)
+        printf("%d ", parts[i]);
+    printf("\n");
+
+    parse_points(content + 44 + (4 * parts_points[0]), parts_points[1]);
+
+}
+
+void parse_points(unsigned char *buf, int num_points)
+{
+    for (int i = 0; i < num_points; i++, buf += 16)
+    {
+        double xy[2];
+        parse_double(buf, xy, 2);
+        printf("Point[%d]\tX\t%f\tY\t%f\n", i + 1, xy[0], xy[1]);
+    }
 }
 
 void parse_int32(unsigned char *buf, int *p, int n, int big_endian)
